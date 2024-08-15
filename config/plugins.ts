c9b89ac8917d2e1a859aed238b0f8b6a0e23c915
cayildiz <cayildiz@innova.com.tr>
2024-08-16 00:57:15 +0300

    AUTH implement edildi bazı hatalar mevcut

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'myemail@protonmail.com',
+        defaultReplyTo: 'myemail@protonmail.com',
+      },
+    },
+  },
+  // ...
+});
